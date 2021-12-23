39c2ac46944e2d166f244aea6bf6f98e516e5c10
Andreas Waldenburger <andreas@waldenburger.name>
2021-12-23 23:20:50 +0100

    my solution to chapter 5

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,38 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, DelayMs, ToggleableOutputSwitch, OutputSwitch};
+use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let step_ms: u16 = 200;
+    let volatile_step_ms = Volatile::new_read_only(&step_ms);
+
+    // Setup initial configuration
+    leds[7].on().ok();
+    leds[0].on().ok();
+    leds[1].on().ok();
+
+    let mut flips = FLIPS.iter().cycle();
+
+    loop {
+        let (l, r) = flips.next().unwrap();
+        leds[*l].toggle().ok();
+        leds[*r].toggle().ok();
+        delay.delay_ms(volatile_step_ms.read());
+    }
 }
+
+const FLIPS: [(usize, usize); 8] = [
+    (7, 2),
+    (0, 3),
+    (1, 4),
+    (2, 5),
+    (3, 6),
+    (4, 7),
+    (5, 0),
+    (6, 1),
+];

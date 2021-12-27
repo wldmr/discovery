31765de3a5c79cc5fc485094b0ce3edf416044a2
Andreas Waldenburger <andreas@waldenburger.name>
2021-12-27 16:44:16 +0100

    My solution for Chapter 08, LEDs again

--- f3discovery/src/08-leds-again/src/main.rs
+++ f3discovery/src/08-leds-again/src/main.rs
@@ -8,19 +8,32 @@ use aux8::entry;
 fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
-    // TODO initialize GPIOE
+    // Setup GPIO E (enable and set as outputs)
+    // GPIO E 8–15 are connected to the LEDs
+    rcc.ahbenr.write(|it| it.iopeen().set_bit());
+    gpioe.moder.write(|it| it
+        .moder8().output()
+        .moder9().output()
+        .moder10().output()
+        .moder11().output()
+        .moder12().output()
+        .moder13().output()
+        .moder14().output()
+        .moder15().output()
+    );
+
 
     // Turn on all the LEDs in the compass
-    gpioe.odr.write(|w| {
-        w.odr8().set_bit();
-        w.odr9().set_bit();
-        w.odr10().set_bit();
-        w.odr11().set_bit();
-        w.odr12().set_bit();
-        w.odr13().set_bit();
-        w.odr14().set_bit();
-        w.odr15().set_bit()
-    });
+    gpioe.odr.write(|it| it
+        .odr8().set_bit()
+        .odr9().set_bit()
+        .odr10().set_bit()
+        .odr11().set_bit()
+        .odr12().set_bit()
+        .odr13().set_bit()
+        .odr14().set_bit()
+        .odr15().set_bit()
+    );
 
     aux8::bkpt();
 

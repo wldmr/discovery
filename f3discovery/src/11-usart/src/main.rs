73c135c2e51c1286444cbdaa017cfab2b5da989e
Andreas Waldenburger <andreas@waldenburger.name>
2021-12-29 12:24:14 +0100

    Chapter 11 - Buffer overrun

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -6,12 +6,15 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
-
+    let bytes = "\n\rThe quick brown fox jumps over the lazy dog.".bytes();
+    let start = mono_timer.now();
+    for b in bytes {
+        while usart1.isr.read().txe().bit_is_clear() {}
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(b)));
+    }
+    let elapsed = start.elapsed();
+    iprintln!(&mut itm.stim[0], "loop took {} ticks ({} us)", elapsed, elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6);
     loop {}
 }

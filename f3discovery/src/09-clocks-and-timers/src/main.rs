22f145ea5623f59c6ce2f976eb10407fd790292d
Andreas Waldenburger <andreas@waldenburger.name>
2021-12-29 11:21:52 +0100

    Chapter 09, Clocks and Timers

--- f3discovery/src/09-clocks-and-timers/src/main.rs
+++ f3discovery/src/09-clocks-and-timers/src/main.rs
@@ -1,29 +1,71 @@
 #![no_main]
 #![no_std]
 
-use aux9::{entry, switch_hal::OutputSwitch, tim6};
+use aux9::{entry,
+           switch_hal::StatefulOutputSwitch,
+           switch_hal::ToggleableOutputSwitch,
+           tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    tim6.arr.write(|w| w.arr().bits(ms));
+    tim6.cr1.write(|w| w.cen().set_bit());
+    while !tim6.sr.read().uif().bit_is_set() {}
+    tim6.sr.write(|w| w.uif().clear_bit())
 }
 
 #[entry]
 fn main() -> ! {
-    let (leds, rcc, tim6) = aux9::init();
-    let mut leds = leds.into_array();
+    let (ledes, rcc, tim6) = aux9::init();
+    let mut leds = ledes.into_array();
 
-    // TODO initialize TIM6
+    // initialize TIM6
+    rcc.apb1enr.write(|w| w
+        .tim6en().set_bit()
+    );
+    tim6.cr1.write(|w| w
+        .opm().set_bit()
+        .cen().clear_bit()
+    );
+    tim6.psc.write(|w| w
+        .psc().bits(7999)
+    );
 
-    let ms = 50;
+    let mut patterns = PATTERNS.iter().cycle();
     loop {
-        for curr in 0..8 {
-            let next = (curr + 1) % 8;
+        let pattern = patterns.next().expect("Patterns should cycle forever.");
+        pattern.apply_to(&mut leds);
+        delay(tim6, 100);
+    }
+}
 
-            leds[next].on().unwrap();
-            delay(tim6, ms);
-            leds[curr].off().unwrap();
-            delay(tim6, ms);
+struct Pattern(u8);
+
+impl Pattern {
+    fn is_bit_set(&self, index: usize) -> bool {
+        self.0 & (1 << index) != 0
+    }
+
+    fn apply_to<T>(&self, leds: &mut [T])
+        where T: StatefulOutputSwitch + ToggleableOutputSwitch
+    {
+        for (index, led) in leds.iter_mut().enumerate() {
+            let is_on = led.is_on().unwrap_or_default();
+            let should_be_on = self.is_bit_set(index);
+            if is_on != should_be_on {
+                led.toggle().unwrap_or_default();
+            }
         }
     }
 }
+
+const PATTERNS: [Pattern; 8] = [
+    Pattern(0b10000011),
+    Pattern(0b00000111),
+    Pattern(0b00001110),
+    Pattern(0b00011100),
+    Pattern(0b00111000),
+    Pattern(0b01110000),
+    Pattern(0b11100000),
+    Pattern(0b11000001),
+];
